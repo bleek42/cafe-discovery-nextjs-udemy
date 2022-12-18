41dc7bb1d5c21986d30597b4fbb7e03fd01b8632
bleek42 <brandonleek42@gmail.com>
2022-12-18 16:59:10 -0500

    changed next.config.js, added footer, got rid of boilerplate

---
+++ src/pages/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<pre>@bleek42: v0.0.1</pre>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
+import Footer from './components/footer';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Cafe Explorer</title>
+				<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+			</Head>
+			<main></main>
+			<Footer />
+		</>
+	);
+}
